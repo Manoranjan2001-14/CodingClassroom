df748c70e44a98e998511cb9bafa8e3613046ab1
Manoranjan2001-14 <1111manoranjan@gmail.com>
2026-01-12 09:34:57 +0530

    Math for DSA

--- Math_DSA/src/com/coding/Odd_Even.java
+++ Math_DSA/src/com/coding/Odd_Even.java
@@ -1,4 +1,11 @@
 package com.coding;
 
 public class Odd_Even {
+    public static void main(String[] args) {
+        int n = 13;
+        System.out.println(isOdd(n));
+    }
+    private static boolean isOdd(int n){
+        return (n & 1) == 1;
+    }
 }
