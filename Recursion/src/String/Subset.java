17ce9660063ad47af17fb83f50eb96dd4f08981c
Manoranjan2001-14 <1111manoranjan@gmail.com>
2026-01-15 09:49:43 +0530

    String with Recursion

---
+++ Recursion/src/String/Subset.java
@@ -0,0 +1,16 @@
+package String;
+
+public class Subset {
+    public static void main(String[] args) {
+        subset(" ", "abc");
+    }
+    static void subset(String p, String up){
+        if(up.isEmpty()){
+            System.out.print(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        subset(p + ch, up.substring(1));
+        subset(p, up.substring(1));
+    }
+}
