7d5c03c93591d40245a173b340564de939f653d6
Manoranjan2001-14 <1111manoranjan@gmail.com>
2026-01-16 10:31:39 +0530

    Recursion sting Permutations

--- Recursion/src/String/Subset.java
+++ Recursion/src/String/Subset.java
@@ -1,8 +1,14 @@
 package String;
 
+import java.util.ArrayList;
+
 public class Subset {
     public static void main(String[] args) {
-        subset(" ", "abc");
+//        subset(" ", "abc");
+//        System.out.println(subsetReturn(" ", "abc"));
+       // subsetAscii(" ", "abc");
+
+        System.out.println(subsetReturnAscii(" ", "abc"));
     }
     static void subset(String p, String up){
         if(up.isEmpty()){
@@ -12,5 +18,47 @@ public class Subset {
         char ch = up.charAt(0);
         subset(p + ch, up.substring(1));
         subset(p, up.substring(1));
+    }
+    static ArrayList<String> subsetReturn(String p, String up){
+        if(up.isEmpty()){
+           ArrayList<String> list = new ArrayList<>();
+           list.add(p);
+           return list;
+        }
+        char ch = up.charAt(0);
+        ArrayList<String> left = subsetReturn(p + ch, up.substring(1));
+        ArrayList<String> right = subsetReturn(p, up.substring(1));
+
+        left.addAll(right);
+
+        return left;
+    }
+
+    static void subsetAscii(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        subsetAscii(p + ch, up.substring(1));
+        subsetAscii(p, up.substring(1));
+        subsetAscii(p+(ch+0), up.substring(1));
+    }
+
+    static ArrayList<String> subsetReturnAscii(String p, String up){
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+        ArrayList<String> first = subsetReturnAscii(p + ch, up.substring(1));
+        ArrayList<String> second = subsetReturnAscii(p, up.substring(1));
+        ArrayList<String> third = subsetReturnAscii(p+(ch+0), up.substring(1));
+
+        first.addAll(second);
+        first.addAll(third);
+
+        return first;
     }
 }
